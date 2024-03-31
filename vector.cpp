9b38b32395c2397796ed78b4a5a62063c62fcda2
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-03-31 15:54:38 +0700

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -31,7 +31,9 @@
      vector<int>::iterator it= v.begin();
      for(it=v.begin();it!= v.end();it++){
              cout<<*it <<" ";
-
+ // C2
+      for(vector<int>::iterator it=v.begin();it!= v.end();it++){
+             cout<<*it <<" ";
      }
 //--------Dung Auto duyet mang--------------------
 vector<int> v={1,2,3,4,5};
