023d8330d7c8aabaaba5268d96db031363cb54a8
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-03-31 18:33:31 +0700

    Update function_blobalo.c

--- function_blobalo.c
+++ function_blobalo.c
@@ -9,3 +9,21 @@ bool kiem_tra_ngto(int n){
         return true;
     }
 }
+//------Xoa phan tu tai vi tri bang k-------------------
+int x=0;
+   for(int i=0;i<n;i++){
+       if(a[i]==k){
+           n--;
+           for(int j=i;j<n;j++){
+               a[j]=a[j+1];
+           }
+           x=1;
+           break;
+           
+       }
+   }
+   if(x){
+       for(int i=0;i<n;i++){
+           cout<<a[i]<<" ";
+       }
+   }
