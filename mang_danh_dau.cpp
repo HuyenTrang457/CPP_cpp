bd197438f329e95b65167462cc6bc4a7f53a9aa5
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-03-31 19:17:52 +0700

    Update mang_danh_dau.cpp

--- mang_danh_dau.cpp
+++ mang_danh_dau.cpp
@@ -31,3 +31,53 @@ int main(){
     }
     return 0;
 }
+
+
+/*hãy tìm giá trị có số lần xuất hiện nhiều nhất trong mảng,
+nếu có nhiều giá trị có cùng số lần xuất hiện thì lấy số có giá trị nhỏ nhất */
+#include<iostream>
+using namespace std;
+int main(){
+    int x[100];
+    int n;
+    cout<<"n= "; cin>>n;
+    cout<<"nhap mang: ";
+    for(int i=0;i<n;i++){
+        cin>>x[i];
+    }
+    /*int dem=0;
+    int a[100]={0}; // mang danh dau
+    for(int i=0;i<n;i++){
+        dem=0;
+        if(!a[x[i]]){
+            dem++;
+            a[x[i]]=1;
+        }
+    }*/
+    int a[100]={0};
+    int dem[100];
+    for(int i=0;i<n;i++){
+        dem[x[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(dem[x[i]]!=0&&(!a[x[i]]))
+        {cout<<x[i]<<": " <<dem[x[i]]<<endl;
+            a[x[i]]=1;
+        }
+    }
+    int max=0;
+    int index=0;
+    int min=100;
+    for(int i=0;i<n;i++){
+        if(max<dem[x[i]]){
+            max=dem[x[i]];
+            
+            min=x[i];
+        }
+        else if(max==dem[x[i]]){
+            if(min>x[i]) min=x[i];
+        }
+    }
+    cout<<endl<< min;
+    return 0;
+}
