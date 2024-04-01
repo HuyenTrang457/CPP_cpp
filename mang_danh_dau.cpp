92905009d4cf60c0356b1aefa7800c54a5e84f09
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-01 11:27:45 +0700

    Update mang_danh_dau.cpp

--- mang_danh_dau.cpp
+++ mang_danh_dau.cpp
@@ -111,3 +111,38 @@ int main(){
     }
     return 0;
 }
+--------------Chèn mảng B vào mảng A bắt đầu từ vị trí K------------------------------
+     
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+    int A[100],B[100];
+    int nA,nB;
+    int k=2;
+    cout<<"nA= "; cin>>nA;
+    cout<<"nhap mang A: ";
+    for(int i=0;i<nA;i++){
+        cin>>A[i];
+    }
+    cout<<"nB= "; cin>>nB;
+    cout<<endl<<"nhap mang B: ";
+    for(int i=0;i<nB;i++){
+        cin>>B[i];
+    } 
+    // Di chuyển các phần tử của mảng A sang phải để tạo khoảng trống để chèn mảng B
+    for(int i=k;i<nA;i++){
+        A[i+nB]=A[i];
+    }
+    // Chép các phần tử của mảng B vào mảng A từ vị trí k
+
+    for(int i=0;i<nB;i++){
+        A[i+k]=B[i];
+    }
+    nA+=nB;
+    for(int i=0;i<nA;i++){
+        cout<<A[i];   
+    }
+    return 0;
+}
