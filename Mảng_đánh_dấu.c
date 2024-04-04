92e37b0ccbbea34491e9225e46f8168618d59302
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-04 14:03:54 +0700

    Update Mảng_đánh_dấu.c

--- Mảng_đánh_dấu.c
+++ Mảng_đánh_dấu.c
@@ -22,3 +22,35 @@
      }
      return 0;
  }
+
+--------------------
+#include <iostream>
+#include<climits>
+using namespace std;
+void swap(int &a,int &b){
+    int tmp=a;
+    a=b;
+    b=tmp;
+}
+int find_max_le(int a[], int n, int k){
+    int max= INT_MIN;
+    for(int i=k;i<n;i++){
+        if(a[i]%2==1&&max<a[i]) max=a[i];
+    }
+    return max;
+}
+int main()
+{
+    int n=10;
+    int a[n]={1,2, 3, 9, 7, 4, 8, 6, 10, 5};
+    int j=0;
+    for(int i=0;i<n;i++){
+        int x=find_max_le(a,n,j);
+        swap(x,a[j]);
+        j++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
