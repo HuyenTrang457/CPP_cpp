88ee2da70156785679a3e7118df61929b82ddeb3
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-05 07:49:39 +0700

    Create vector_bt.cpp

---
+++ vector_bt.cpp
@@ -0,0 +1,31 @@
+-----------------EX1--------------------------------
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    int n; cin>>n;
+    for(int i=0;i<n;i++){
+        int op; cin>>op;
+        if(op==1)
+        {
+            int tmp;
+            cin>>tmp;
+            vec.push_back(tmp);
+        } else if(op==2){
+            if(!vec.empty()){
+                vec.pop_back();
+            }
+            
+        }
+    }
+    if(vec.empty()){
+        cout<<"EMPTY";
+    }else{
+        for(int i=0;i<n;i++){
+            cout<<vec[i]<<endl;
+        }
+    }
+    return 0;
+}
