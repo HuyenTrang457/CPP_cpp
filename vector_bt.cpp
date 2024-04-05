7244e9fc9d87c7ab213a69818501905550496cd2
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-05 08:49:12 +0700

    Update vector_bt.cpp

--- vector_bt.cpp
+++ vector_bt.cpp
@@ -29,3 +29,46 @@ int main()
     }
     return 0;
 }
+
+-----------------EX2: ERASE - INSERT-------------------------------
+    /*Thao tác 1 : Chèn phần tử vào vị trí bất kì trong vector.
+Thao tác 2 : Xóa phần tử ở vị trí bất kì trong vector
+*/
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    int m; cin>>m; //số lượng phần tử vecto
+    for(int i=0;i<m;i++){
+        int tmp;
+        cin>>tmp;
+        vec.push_back(tmp);
+    }
+    int n; cin>>n; // số thao tác sẽ thực hiện
+    for(int i=0;i<n;i++){
+        int op; cin>>op;
+        if(op==1){
+            int pos, val;
+            cin>>pos>>val;
+             vec.insert(vec.begin() + pos, val);
+        }else if(op==2){
+            if(!vec.empty())
+            {int pos;
+            cin>>pos;
+            vec.erase(vec.begin()+pos);}
+        }
+    }
+        if(vec.empty()){
+            cout<<"EMPTY";
+            
+        }else{
+            for(int x : vec){
+                cout<<x<<" ";
+            }
+        }
+    
+    
+    return 0;
+}
