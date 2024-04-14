4b8522d9bfc473bea4964494b391121a55444b0b
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-15 06:12:42 +0700

    Update STRING.cpp

--- STRING.cpp
+++ STRING.cpp
@@ -1,1 +1,7 @@
-transform(s.begin(),s.end(),s.end(), ::tolower());
+transform(s.begin(),s.end(),s.end(), ::tolower);
+
+for( char &x:s) x= tolower(x);
+
+n=1234
+s= ntoi (n) // string to integer
+n= stoll (s)  // string to long long
