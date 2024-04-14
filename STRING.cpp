6c9b0fee6d02876988a1f7db61ff516376f90066
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-15 06:23:09 +0700

    Update STRING.cpp

--- STRING.cpp
+++ STRING.cpp
@@ -5,4 +5,4 @@ for( char &x:s) x= tolower(x);
 n=1234
 s= ntoi (n) // string to integer
 n= stoll (s)  // string to long long
-reverse( s.begin(), s.end()); // lật ngược sâu
+reverse( s.begin(), s.end()); // lật ngược sâu, áp dụng cho cả mảng và vector
