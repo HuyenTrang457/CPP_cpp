0057cbdc88bdd6f152ea8a6c400fb27c73c98b90
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-15 14:10:23 +0700

    Update STRING.cpp

--- STRING.cpp
+++ STRING.cpp
@@ -6,3 +6,12 @@ n=1234
 s= ntoi (n) // string to integer
 n= stoll (s)  // string to long long
 reverse( s.begin(), s.end()); // lật ngược sâu, áp dụng cho cả mảng và vector
+-------------------
+  string s; 
+    getline(cin,s);
+    stringstream ss(s);
+    string word;
+    while(ss>> word)
+    {
+        cout<<word<<endl;
+    }
