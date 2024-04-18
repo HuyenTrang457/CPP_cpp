d5e4fec7fd8c25fa9158514dd6abbe411a1ce6b8
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-18 10:43:36 +0700

    Update STRING.cpp

--- STRING.cpp
+++ STRING.cpp
@@ -6,6 +6,7 @@ n=1234
 s= ntoi (n) // string to integer
 n= stoll (s)  // string to long long
 reverse( s.begin(), s.end()); // lật ngược sâu, áp dụng cho cả mảng và vector
+s.find(k[i])!= string::npos // tìm kiếm
 -------------------
   string s; 
     getline(cin,s);
