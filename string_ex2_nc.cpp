11b27bbacf1a8c619fc34711eae8b810e7422762
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-19 08:46:58 +0700

    Update string_ex2_nc.cpp

--- string_ex2_nc.cpp
+++ string_ex2_nc.cpp
@@ -38,3 +38,68 @@ int main(){
    }
     return 0;
 }
+
+
+--------------------------------------------------------------------------
+ /* Cho số tự nhiên m và số nguyên s không âm. Nhiệm vụ của bạn là tìm số bé nhất 
+ và lớn nhất có m chữ số và tổng chữ số bằng s.
+*/
+ #include<iostream>
+ #include<ctype.h>
+ #include<algorithm>
+ #include<vector>
+#include <bits/stdc++.h>
+#include<sstream>
+using namespace std;
+bool compare(string &a, string &b){
+    return a>b;
+}
+int sum(int n) // hàm tính tổng chữ số của n
+{
+    string x= to_string(n);
+    int sum=0;
+    for(int i=0;i<x.size();i++){
+        sum=sum+ x[i]-'0';
+    }
+    return sum;
+}
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    string s1;
+    string s2;
+    int p;
+    int n;
+    fin>>n>>p;
+    //getline(fin, s1);
+    string strbegin="1"; // số nhỏ nhất đầu tiên có n chữ số
+    for(int i=1;i<n;i++){
+        strbegin+="0";
+    }
+    int ibegin= stoi(strbegin);
+    string strend="";
+    for(int i=0;i<n;i++){
+        strend+="9";
+    }
+    int iend= stoi(strend);
+    int s=0;
+     for(int i=ibegin;i<=iend;i++){
+         s=sum(i);
+         if(s==p){
+             cout<<i<<endl;
+             break;
+         }
+     }
+     for(int i=iend;i>=ibegin;i--){
+         s=sum(i);
+         if(s==p){
+             cout<<i;
+             break;
+         }
+     }
+    
+    fin.close();
+    fout.close();
+    return 0;
+}
