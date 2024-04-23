9785b824c23bce3e2ab03bd44fa7222091290c6f
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-23 13:46:56 +0700

    Update Loang.cpp

--- Loang.cpp
+++ Loang.cpp
@@ -102,3 +102,58 @@ int main()
     fout.close();
     return 0;
 }
+
+
+
+
+------------------------------------------------------------------------
+//ĐƯỜNG ĐI QUÂN TỊNH
+    //xác định xem số lượng ô trên bàn cờ mà quân Tịnh có thể di chuyển
+//tới, biết rằng nó có thể đi qua đi lại 1 ô trống nhiều lần và không thể đi vào ô có vật cản.
+
+    #include <bits/stdc++.h>
+using namespace std;
+using ll= long long;
+int n, m,s,t;
+int a[100][100];
+/* TRường hợp tính cả  chung đỉnh
+    int dx[8]={-1,0,1,1,1,0,-1,-1};
+    int dy[8]={-1,-1,-1,0,1,1,1,0};
+
+    Quân mã
+    int dx[8]={-2,-2,-1,1,2,2,1,-1};
+    int dy[8]={-1,1,2,2,1,-1,-2,-2};
+*/
+int dx[4]={-1,-1,1,1};
+int dy[4]={-1,1,-1,1};
+int dem=0;
+void loang(int i, int j){
+    a[i][j]=1;
+    ++dem;
+    for(int k=0;k<4;k++){
+        int i1= i +dx[k], j1= j+dy[k];
+        if(i1>=0 && i1<n && j1>=0 && j1<m && a[i1][j1]==0){
+            loang(i1,j1);
+        }
+    }
+}
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    fin>>n>>m;
+    fin>>s>>t;
+    s--;t--;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            fin>> a[i][j];
+        }
+    }
+    loang(s,t);
+    cout<<dem;
+    fin.close();
+    fout.close();
+    return 0;
+}
+
+
