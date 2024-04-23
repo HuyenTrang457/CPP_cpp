3d6e4510f7f0759eb76698bb0f93dd6b14b6a6e8
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-23 14:56:02 +0700

    Update Đồng_Dư .cpp

--- Đồng_Dư .cpp
+++ Đồng_Dư .cpp
@@ -4,7 +4,10 @@ int res=0;
         res %= 100;
     }
 
-// Tìm K chữ số cuối cùng của số N^M
-// ----> chia dư N^M cho 10^k
-
-int res= 
+// Tìm K chữ số cuối cùng của n^m --> Chia dư n^m cho 10^k
+    k=pow(10,k);
+    int res=1;
+    for(int i=0;i<m;i++){
+        res*= n;
+        res %= k;
+    } 
