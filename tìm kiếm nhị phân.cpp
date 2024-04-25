30b710602d882a83d3ba33b86c9f1a46c3238d8b
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-25 15:40:45 +0700

    Create tìm kiếm nhị phân.cpp

---
+++ tìm kiếm nhị phân.cpp
@@ -0,0 +1,17 @@
+// lower_bound : tìm kiếm  phần tử nhỏ nhất lớn hơn hoặc bằng giá trị x trong mảng, vector (đã sắp xếp)
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+vector<int> a = {1, 2, 3, 3, 3, 4, 6};
+auto it = lower_bound(a.begin(), a.end(), 3);
+if(it == a.end()){
+  cout << "NOT FOUND\n";
+  }
+else{
+  cout << *it << endl;
+  }
+  cout << (it - a.begin()) << endl;
+}
+
+// upper_bound : tìm kiếm phần tử nhỏ nhất lớn hơn giá trị x... ( khác lower_bound ở dấu =x)
