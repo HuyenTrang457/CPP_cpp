423888a473dac982fd290ec110da5a6a629e2e8b
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-28 01:11:07 +0700

    Update exercise. binary search.cpp

--- exercise. binary search.cpp
+++ exercise. binary search.cpp
@@ -91,3 +91,55 @@ int main()
     cout<<n-m+1;
     return 0;
 }
+
+
+
+-----------------------------------------------------------------------------------------
+[Sắp xếp - Tìm Kiếm]. Bài 27. Số nhỏ nhất lớn hơn A[i]
+    
+    #include <bits/stdc++.h>
+using namespace std;
+int vi_tri_dau_tien_lon_hon(int a[], int n, int k){
+   
+    int l=0, r=n-1, x=-1;
+    a[-1]=-1;
+    while(l<=r){
+        int tmp=(r+l)/2;
+        if (a[tmp] > k) {
+            x = tmp; 
+            r= tmp-1;
+        }
+        else if(a[tmp]<=k){
+            l= tmp+1;
+        }
+        
+        
+    }
+    return a[x];
+}
+
+int main() {
+    
+    int a[9] ={6, 3, 9, 8, 10, 2, 1, 15, 7};  //7 6 10 9 15 3 2 _ 8
+    int n=9;
+    vector<int> ve;
+    for(int x:a){
+       ve.push_back(x);
+    }
+    int x;
+    int b[100];
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+       b[a[i]]= vi_tri_dau_tien_lon_hon(a,n,a[i]);
+    }
+    for(int x:ve){
+       cout<<x<<"   ";
+       if(b[x]!=-1) cout<<b[x];
+       else{
+          cout<<"_";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+
