62426af7b22ecb6e855de8f55ae7a888a0661689
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-05 21:00:09 +0700

    Update SINH .cpp

--- SINH .cpp
+++ SINH .cpp
@@ -1,3 +1,7 @@
+## Kiểm tra lại bài đầu ( Sinh phân hoạch)
+
+
+
 --> thư viện: <algorithm>
 next_permutation(a,a+n)  ---> trả về cấu hình kế tiếp của mảng a
 cout<<  next_permutation(a,a+n)  ----> trả về TRue( nếu vẫn CÓ THỂ sinh ra cấu hình kế tiếp)
@@ -16,6 +20,63 @@ int main() {
 	} while (next_permutation(a + 1, a + n + 1));
 }
 
+
+
+
+-----------------SINH PHÂN HOẠCH----------------------------------------
+	
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using ll = long long;
+using namespace std;
+int n, a[100], final = 0;
+int cnt;
+void ktao() {
+	cnt = 1;
+	a[1] = n;
+}
+
+void sinh() {
+	// bat dau tu so hang cuoi cung va tim so hạng đầu tiên khác 1
+	int i = cnt;
+	while (i >= 1 && a[i] == 1) {
+		--i;
+	}
+	if (i == 0) final = 1;
+	else {
+		a[i]--;
+		int d = cnt - i + 1;
+		// cnt-i: số lượng số 1 đã bỏ qua -->  cnt-i+1 : cộng 1 để cộng lại cái giá trị mà a[i] đã trừ ở ngay phía trên
+		
+		// ---> biểu diễn d theo a[i]
+		cnt = i;
+		int q = d / a[i]; // tính d gấp mấy lần a[i]
+		int r = d % a[i];
+		for (int j = 1;  j <= q; j++) {
+			a[i + j] = a[i];
+			++cnt;
+		}
+		if (r != 0) {
+			++cnt;
+			a[cnt ] = r; // kiểm tra lại
+		}
+	}
+
+}
+
+int main() {
+	cin >> n;
+	ktao();
+	while (!final) {
+		for (int i = 1; i <= n; i++) {
+			cout << a[i];
+		}
+		cout << endl;
+		sinh();
+	}
+}
+------------------------------------------------------------------------------------
 
 ## Sinh nhị phân
 
