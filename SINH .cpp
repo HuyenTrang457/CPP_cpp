4b65c20b32117134145e6fa81c420d9a8a5d8f08
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-05 13:30:08 +0700

    Update SINH .cpp

--- SINH .cpp
+++ SINH .cpp
@@ -33,3 +33,47 @@ int main() {
 	}
 
 }
+
+
+
+---------------- SINH TỔ HỢP-----------------------------------------------------------------
+#include<iostream>
+using namespace std;
+using ll = long long;
+int n, k, final = 0;
+int a[100];
+
+void ktao() {
+	for (int i = 1; i <= k; i++) {
+		a[i] = i;
+	}
+}
+
+void sinh() {
+	int i = k;
+	while (i != 0 && a[i] == n - k + i) {
+		i--;
+	}
+	if (i == 0) {
+		final = 1;
+	}
+	else // a[i] != n-k+i (a[i] chua dat max
+	{
+	   a[i]++;
+		for (int j = i+1; j <= k; j++) {
+			a[j] = a[j - 1] + 1;
+		}
+	}
+}
+int main() {
+		cin >> n >> k;
+		ktao();
+		while (!final) {
+			for (int i = 1; i <= k; i++) {
+				cout << a[i];
+			}
+			cout << endl;
+			sinh();
+		}
+}
+
