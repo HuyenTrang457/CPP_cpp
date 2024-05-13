6a307dc8fed7a2c7ba264cd268754cecfbb19ec4
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-13 14:14:14 +0700

    Create Quay Lui.cpp

---
+++ Quay Lui.cpp
@@ -0,0 +1,29 @@
+--------- NHỊ PHÂN---------------------
+
+#include<iostream>
+using namespace std;
+
+int a[1000], n;
+void in() {
+	for (int j = 1; j <= n; j++) {
+		cout << a[j];
+	}
+}
+void Try(int i)
+{
+	
+	for (int j = 0; j <= 1; j++) {
+		a[i] = j;
+		if (i == n) {
+			in();
+			cout << endl;
+		}
+		else {
+			Try(i + 1);
+		}
+	}
+}
+int main() {
+	cin >> n;
+	Try(1);
+}
