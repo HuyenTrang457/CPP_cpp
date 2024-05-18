ac431cd859b1a49a54e548e17091aaa14df32fd3
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-18 17:22:05 +0700

    Update Quay Lui.cpp

--- Quay Lui.cpp
+++ Quay Lui.cpp
@@ -27,3 +27,27 @@ int main() {
 	cin >> n;
 	Try(1);
 }
+
+----------------------------------------
+// Sinh to hop chap k cua n
+#include<iostream>
+using namespace std;
+int a[100], n,k; // to hop k cua n phan tu
+int mark[100] = { 0 };
+void inkq() {
+	for (int i = 1; i <= n; i++) {
+		if(a[i]) cout << a[i];
+	}
+	cout << endl;
+}
+void sinh(int i) {
+	for(int j=a[i-1]+1;j<=n-k+i;j++){
+	   a[i]=j;
+	   if(i==k) inkq();
+	   else sinh(i+1);
+	}
+}
+int main() {
+	n = 6; k = 4;
+	sinh(1);
+}
