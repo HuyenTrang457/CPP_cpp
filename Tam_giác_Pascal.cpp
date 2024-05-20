92b9c7a10c41c860d50cddab1f2fddb452dc5139
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-20 08:21:04 +0700

    Update Tam_giác_Pascal.cpp

--- Tam_giác_Pascal.cpp
+++ Tam_giác_Pascal.cpp
@@ -25,3 +25,27 @@ int main() {
 	cin >> n;
 	xuat_tam_giac_pascal(n);
 }
+
+
+---------Đệ quy
+	#include<iostream>
+using namespace std;
+int pascal(int n,int k) {
+	if(k==0||k==n) return 1;
+	return pascal(n - 1, k - 1) + pascal(n - 1, k);
+}
+
+void xuat_tam_giac_pascal(int n)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << pascal(i, j) << " ";
+		}
+		cout << endl;
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+	xuat_tam_giac_pascal(n);
+}
