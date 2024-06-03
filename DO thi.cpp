6170f98e4a3e45cfbe9f7ef052fa18a5c2809b45
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-06-03 14:37:20 +0700

    Create DO thi.cpp

---
+++ DO thi.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> ke[101];
+int a[101][101];
+bool visited[101]={false};
+int n,m;
+
+void DFS1(int u){
+   cout<<u<<" ";
+   visited[u]= true;
+   for(int v:ke[u]){
+      if(!visited[v]) DFS1(v);
+      
+   }
+}
+
+void DFS2(int u)
+int main()
+{
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+       int x,y; cin>>x>>y;
+       ke[x].push_back(y);
+       ke[y].push_back(x);
+       
+    }
+   for(int i=1;i<=n;i++){
+      sort(ke[i].begin(),ke[i].end());
+      cout<<i<<": ";
+      for(int x:ke[i]) cout<<x<< " ";
+      cout<<endl;
+   }
+   DFS1(1);
+    return 0;
+}
